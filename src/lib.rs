2ee5e62714071138cd45b5d8564da57aa70cb2cd
Carlos Menezes <carlos-menezes212@hotmail.com>
2021-06-19 19:51:13 +0100

    Add Simpson's First Law (1/3) - adds tests - fix `trapezoid` test method name

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 pub mod limit;
 pub mod trapezoid;
+pub mod simpson;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     use limit::IntgrLimit;
-    use trapezoid::{ trapezoid };
+    use trapezoid::trapezoid;
+    use simpson::one_third;
     
     #[test]
-    fn test_fn_1() {
+    fn test_trapezoid() {
         let res = trapezoid(|x: f32| (x.powi(4) / 4.0) + x.powi(2) + x.sin(), IntgrLimit { a: 0.0, b: std::f32::consts::PI }, 6);
         assert_eq!(res, 28.44046783447265625000000000000000);
+    }
+    
+    #[test]
+    fn test_simpson_one_third() {
+        let res = one_third(|x: f32| (x.powi(4) / 4.0) + x.powi(2) + x.sin(), IntgrLimit { a: 0.0, b: std::f32::consts::PI }, 6);
+        assert_eq!(res, 27.64514541625976562500000000000000);
     }
 }
---
+++ src/simpson.rs
@@ -0,0 +1,20 @@
+use crate::limit::IntgrLimit;
+
+pub fn one_third<F>(f: F, l: IntgrLimit, n: i32) -> f32 where
+F: Fn(f32) -> f32 {
+    let h: f32 = (l.b - l.a) / n as f32;
+
+    let mut s1: f32 = 0.0; // for odd `i`
+    for i in (1..n).step_by(2) {
+        let x = l.a+(h * i as f32);
+        s1 += f(x); // odd x
+    }
+
+    let mut s2: f32 = 0.0; // for even `i`
+    for i in (2..n-1).step_by(2) {
+        let x = l.a+(h * i as f32);
+        s2 += f(x); // odd x
+    }
+
+    return (h / 3.0)*(f(l.a) + 4.0*s1 + 2.0*s2 + f(l.b))
+}
