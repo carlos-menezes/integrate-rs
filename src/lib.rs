145de49a3d86c273f5234923aba63d3106901b6f
Carlos Menezes <carlos-menezes212@hotmail.com>
2021-06-19 20:06:12 +0100

    Add Simpsons Second Rule (3/8)

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ mod tests {
 
     use limit::IntgrLimit;
     use trapezoid::trapezoid;
-    use simpson::one_third;
+    use simpson::{one_third, three_eights};
     
     #[test]
     fn test_trapezoid() {
@@ -20,5 +20,11 @@ mod tests {
     fn test_simpson_one_third() {
         let res = one_third(|x: f32| (x.powi(4) / 4.0) + x.powi(2) + x.sin(), IntgrLimit { a: 0.0, b: std::f32::consts::PI }, 6);
         assert_eq!(res, 27.64514541625976562500000000000000);
+    }
+
+    #[test]
+    fn test_simpson_three_eights() {
+        let res = three_eights(|x: f32| (x.powi(4) / 4.0) + x.powi(2) + x.sin(), IntgrLimit { a: 0.0, b: std::f32::consts::PI }, 6);
+        assert_eq!(res, 27.65612792968750000000000000000000);
     }
 }
--- src/simpson.rs
+++ src/simpson.rs
@@ -18,3 +18,24 @@ F: Fn(f32) -> f32 {
 
     return (h / 3.0)*(f(l.a) + 4.0*s1 + 2.0*s2 + f(l.b))
 }
+
+pub fn three_eights<F>(f: F, l: IntgrLimit, n: i32) -> f32 where
+F: Fn(f32) -> f32 {
+    let h: f32 = (l.b - l.a) / n as f32;
+
+    let mut s1: f32 = 0.0; // for i != 3, 6, 9, ...
+    for i in 1..n {
+        if i % 3 != 0 {
+            let x = l.a+(h * i as f32);
+            s1 += f(x);
+        }
+    }
+
+    let mut s2: f32 = 0.0; // for i = 3, 6, 9, ...
+    for i in (3..n-2).step_by(2) {
+        let x = l.a+(h * i as f32);
+        s2 += f(x);
+    }
+
+    return (3.0 * h / 8.0)*(f(l.a) + 3.0*s1 + 2.0*s2 + f(l.b))
+}
